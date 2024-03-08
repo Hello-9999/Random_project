9138afe361ff2bc8e1d8773f48d573c5fa04700e
Gautam Magar <puskarmagar687@gmail.com>
2024-03-09 01:19:01 +0545

    fix : fixed  element path location

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Client_home from "./pages/client/Client_home";
+import Admin_home from "./pages/admin/Admin Home/Admin_home";
+import Admin_event from "./pages/admin/Admin Event/Admin_event";
+import Admin_addevent from "./pages/admin/Admin Event/Admin_addevent";
 import Admin_login from "./pages/admin/Admin_login";
-import Admin_home from "./pages/admin/Admin_home";
-import Admin_addevent from "./pages/admin/Admin_addevent";
-import Admin_event from "./pages/admin/Admin_event";
-import Admin_member from "./pages/admin/Admin_member";
-import Admin_blog from "./pages/admin/Admin_blog";
-import Admin_gallery from "./pages/admin/Admin_gallery";
-import Admin_addmember from "./pages/admin/Admin_addmember";
+import Admin_member from "./pages/admin/Admin Member/Admin_member";
+import Admin_addmember from "./pages/admin/Admin Member/Admin_addmember";
+import Admin_blog from "./pages/admin/Admin Blog/Admin_blog";
+import Admin_gallery from "./pages/admin/Admin Gallery/Admin_gallery";
 import Admin_Event_Detail from "./pages/admin/Admin_Event_Detail";
 
 function App() {
