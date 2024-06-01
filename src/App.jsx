4081ed6e7b6e6b0c90c2e78a18c130da29553d55
Gautam Magar <puskarmagar687@gmail.com>
2024-06-02 00:13:58 +0545

    Set up routing and protected routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 // import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Client_home from "./pages/client/Client_home";
-import Admin_home from "./pages/admin/Admin Home/Admin_home";
 import Admin_event from "./pages/admin/Admin Event/Admin_event";
 import Admin_addevent from "./pages/admin/Admin Event/Admin_addevent";
 import Admin_login from "./pages/admin/Admin_login";
 import Admin_member from "./pages/admin/Admin Member/Admin_member";
 import Admin_addmember from "./pages/admin/Admin Member/Admin_addmember";
-import Admin_blog from "./pages/admin/Admin Blog/Admin_blog";
 import Admin_gallery from "./pages/admin/Admin Gallery/Admin_gallery";
 import Admin_Event_Detail from "./pages/admin/Admin_Event_Detail";
+import Admin_Home from "./pages/admin/Admin Home/Admin_Home";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Client_home />} />
         <Route path="/iamadmin" element={<Admin_login />} />
-        <Route path="/admin_home" element={<Admin_home />} />
-        <Route path="/admin_event/" element={<Admin_event />} />
-        <Route path="/admin_event/:path" element={<Admin_addevent />} />
-        <Route path="/event_Detail/:uid" element={<Admin_Event_Detail />} />
-        <Route path="/admin_member" element={<Admin_member />} />
-        <Route path="/admin_member/:path" element={<Admin_addmember />} />
-        <Route path="/admin_blog" element={<Admin_blog />} />
-        <Route path="/admin_gallery" element={<Admin_gallery />} />
+
+        <Route path="" element={<ProtectedRoute />}>
+          <Route path="adminhome" element={<Admin_Home />} />
+          <Route path="adminmember" element={<Admin_member />} />
+          <Route path="adminevent/:path" element={<Admin_addevent />} />
+          <Route path="adminmember/:path" element={<Admin_addmember />} />
+          <Route path="eventDetail/:uid" element={<Admin_Event_Detail />} />
+          <Route path="admingallery" element={<Admin_gallery />} />
+        </Route>
       </Routes>
     </>
   );
