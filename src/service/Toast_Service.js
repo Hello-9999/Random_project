66a742db311caaa4da70134320cb728387cf42e1
Gautam Magar <puskarmagar687@gmail.com>
2024-06-01 18:07:29 +0545

    feat: added  Toast

--- src/service/Toast_Service.js
+++ src/service/Toast_Service.js
@@ -1,11 +1,16 @@
 import { toast } from "sonner";
 
 export const error_toast = (msg) => {
-  return toast.success(msg);
+  return toast.error(msg);
 };
 
 export const success_Toast = (msg) => {
   return toast.success(msg);
+};
+export const des_Success_Toast = (title, msg) => {
+  return toast.success(title, {
+    description: msg,
+  });
 };
 
 export const warn_Toast = (msg) => {
