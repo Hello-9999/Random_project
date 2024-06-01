97fcd12df7330e53c8ba7c6721d89e7b80605856
Gautam Magar <puskarmagar687@gmail.com>
2024-06-01 18:04:58 +0545

    feat: Update login form design

--- src/pages/admin/Admin_login.jsx
+++ src/pages/admin/Admin_login.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../service/login_Server";
 import { useSelector, useDispatch } from "react-redux";
 import addinguserinfo, {
   Adding_info,
 } from "../../Redux/Feature/addinguserinfo";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import {
+  des_Success_Toast,
+  error_toast,
+  success_Toast,
+} from "../../service/Toast_Service";
+import { Login_service } from "../../service/Auth_Service";
 
 const Admin_login = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
+  const [loading, setloading] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const Showpassword = (e) => {
     const passText = document.getElementById("password");
 
@@ -22,100 +30,130 @@ const Admin_login = () => {
     }
   };
 
-  const handle_signin = (e) => {
+  const handle_signin = async (e) => {
     e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        const User_data = {
-          ID: user.uid,
+    setloading(true);
 
-          Name: user.displayName,
-          Email: user.email,
-          Email_Verfied: user.emailVerified,
-          Photo: user.photoURL,
-        };
-        console.log(User_data, "data");
-        dispatch(Adding_info(User_data));
-        navigate("/admin_home");
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-
-        console.log(errorCode);
-        if ("auth/invalid-credential" == errorCode) {
-          console.log("You are not an admin. 🛑");
+    const response = await Login_service(email, password);
+    try {
+      setTimeout(() => {
+        if (response) {
+          setloading(false);
+          des_Success_Toast(
+            "Welcome back, Admin",
+            "You've successfully logged in."
+          );
+          const User_data = {
+            ID: response.uid,
+            isLoggedin: true,
+            Name: response.displayName,
+            Email: response.email,
+            Email_Verfied: response.emailVerified,
+            Photo: response.photoURL,
+          };
+          dispatch(Adding_info(User_data));
+          navigate("/admin/home");
         } else {
+          setloading(false);
         }
-        const errorMessage = error.message;
-      });
+      }, 3000);
+    } catch (error) {
+      setTimeout(() => {
+        setloading(false);
+      }, 3000);
+    }
   };
-  return (
-    <div className="login_container bg-slate-600 h-screen  ">
-      <div className="form w-5/6 m-auto pt-5 md:w-3/5 lg:w-2/5">
-        <form
-          onSubmit={handle_signin}
-          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        >
-          <div class="mb-4">
-            <label
-              class="block text-gray-700 text-lg font-bold mb-2 xl:text-xl"
-              for="email"
-            >
-              Email
-            </label>
-            <input
-              class="shadow  appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline h-12 required: "
-              id="email"
-              type="email"
-              placeholder="Email"
-              onChange={(e) => setemail(e.target.value)}
-              required
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-gray-700 text-lg font-bold mb-2 xl:text-xl"
-              for="password"
-            >
-              Password
-            </label>
-            <input
-              class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline h-12  required:"
-              id="password"
-              type="password"
-              placeholder="Password"
-              required
-              onChange={(e) => setpassword(e.target.value)}
-            />
-            <p class="text-red-500 text-xs italic"></p>
 
-            <div className="showPass flex gap-2  items-center">
+  return (
+    <>
+      <div className="login_container bg-slate-600 h-screen  relative">
+        <div
+          className="w-5/6 m-auto pt-5 md:w-3/5 lg:w-2/5 rounded-none md:rounded-2xl p-4 md:p-8 shadow-input absolute left-0 right-0 top-6 md:top-16 lg:top-25"
+          style={{ backgroundColor: "#333333" }}
+        >
+          <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+            Welcome to the Phoenix IT Club Admin Portal
+          </h2>
+          <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            Please enter your credentials to access the admin dashboard. If you
+            experience any issues, contact support for assistance.
+          </p>
+          <form onSubmit={handle_signin} class="my-8">
+            <div class="mb-4">
+              <label
+                class="text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-3"
+                for="email"
+              >
+                Email
+              </label>
               <input
-                type="checkbox"
-                className="size-4"
-                onClick={Showpassword}
+                class="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent
+              file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600
+              focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+               disabled:cursor-not-allowed disabled:opacity-50
+               dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+               group-hover/input:shadow-none transition duration-400 mt-2"
+                id="email"
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setemail(e.target.value)}
+                required
               />
-              <p className="text-black font-medium">Show password</p>
             </div>
-          </div>
-          <div class="flex items-center justify-between">
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign In
-            </button>
-            <a
-              class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
+            <div class="mb-5">
+              <label
+                class="text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-3"
+                for="password"
+              >
+                Password
+              </label>
+              <input
+                class="mt-2 flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent
+              file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600
+              focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+               disabled:cursor-not-allowed disabled:opacity-50
+               dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+               group-hover/input:shadow-none transition duration-400 "
+                id="password"
+                type="password"
+                placeholder="Password"
+                required
+                onChange={(e) => setpassword(e.target.value)}
+              />
+              <p class="text-red-500 text-xs italic"></p>
+
+              <div className="showPass flex gap-2  items-center mt-3">
+                <input
+                  type="checkbox"
+                  className="size-4"
+                  onClick={Showpassword}
+                />
+                <p className="text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ">
+                  Show password
+                </p>
+              </div>
+            </div>
+            <div class="flex items-center  w-full md:w-3/4 lg:w-10/12 justify-between m-auto">
+              {!loading ? (
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline  w-full"
+                  type="submit"
+                >
+                  Sign In
+                </button>
+              ) : (
+                <button
+                  class="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline  w-full cursor-wait animate-pulse mt-3"
+                  disabled
+                >
+                  Please Wait ...
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
