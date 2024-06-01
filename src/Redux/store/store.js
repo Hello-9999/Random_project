2b93f96e41fe59d1bbd9043e94919dc358166b5a
Gautam Magar <puskarmagar687@gmail.com>
2024-06-01 18:08:18 +0545

    feat : Added Redux Persit

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import addinguserinfo from "../Feature/addinguserinfo";
 
-export const store = configureStore({
-  reducer: {
-    User_info: addinguserinfo,
-  },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  User_info: addinguserinfo,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+let persistor = persistStore(store);
+export default persistor;
